33d97d8729da98709a44297a12c8c76d10c458f9
Florian Gilcher <florian.gilcher@asquera.de>
2018-02-21 22:23:04 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate stdweb;
+
+use stdweb::web::{Element,document};
+
+use stdweb::web::event::ClickEvent;
+
+use stdweb::web::IParentNode;
+use stdweb::web::IEventTarget;
+
+fn main() {
+    stdweb::initialize();
+
+    let register_hits_button: Element = document().query_selector( ".press" ).unwrap().unwrap();
+    register_hits_button.add_event_listener(move |_: ClickEvent| {
+       console!(log, "worked!");
+    });
+
+    stdweb::event_loop();
+}
